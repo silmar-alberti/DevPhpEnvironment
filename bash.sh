264f433a7be6d047589326ee26f1dd4b3cacb23b
silmar-alberti <silmar.alberti@yahoo.com>
2017-07-02 13:01:33 -0300

    modificado para executar comando dentro do container em execução

--- bash.sh
+++ bash.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-#este comando executa outra imagem mas nao configura o apache e outros scripts de configuraçao
-docker-compose run blog /bin/bash
+docker-compose exec blog /bin/bash
