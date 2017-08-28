e0d230bef965082c9fdc469f4c4aef3f83f01112
silmar-alberti <silmar.alberti@yahoo.com>
2017-08-28 20:18:08 -0300

    adiocionado configuração padrao de zip json

--- auryn/config/apacheConfig.sh
+++ auryn/config/apacheConfig.sh
@@ -60,3 +60,12 @@ if [ $apcIniCli -eq 0 ]; then
 	echo "Adicionando ACPU no arquivo $phpIniCli"
 	printf $apcText >> $phpIniCli
 fi
+
+echo "configurando compactacao json"
+
+printf "<IfModule mod_deflate.c>
+            <IfModule mod_filter.c>
+                    # these are known to be safe with MSIE 6
+                    AddOutputFilterByType DEFLATE application/json
+             </IfModule>
+        </IfModule>" >> /etc/apache2/mods-enabled/deflate.conf
