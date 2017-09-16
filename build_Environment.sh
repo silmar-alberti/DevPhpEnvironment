24844be2305b926e11614e644b7a15cd45b68b14
silmar-alberti <silmar.alberti@yahoo.com>
2017-09-15 23:45:51 -0300

    melhorado script de build

--- build_Environment.sh
+++ build_Environment.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
-export IP_HOST=$(LANG=C ifconfig docker0 | awk -F: '/inet addr/ {gsub(/ .*/,"",$2); print $2}')
+
+if [[ $EUID -ne 0 ]]; then
+  echo "Sudo! Onde estao os seus poderes de supervaca?! Mas não pode ser root!" 2>&1
+  exit 1;
+fi
+
+clear_file_and_write() {
+        rm ./.env
+        echo 'IP_HOST='$IP_HOST >> ./.env
+}
+
+if [[ "$1" == --interface ]]; then
+    IP_HOST=$(LANG=C ifconfig $2 | awk -F: '/inet addr/ {gsub(/ .*/,"",$2); print $2}');
+    clear_file_and_write
+elif [[ "$1" ==  --ip ]]; then
+    IP_HOST=$2;
+    clear_file_and_write
+else
+    echo 'usando arquivo de configuracao padrao'
+fi
+
 
 cd ubuntuBase
 docker-compose build
